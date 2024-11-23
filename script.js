b46ad2f1182976088bceb819a08f12e3200f2456
manaheel jamil <manaheeljamil20@gmail.com>
2024-11-23 12:09:21 +0500

    initialize

---
+++ script.js
@@ -0,0 +1,50 @@
+// let inputs = document.getElementById("todo-input");
+// let text = document.getElementById("todo-list");
+// function addTodo() {
+//   if (inputs.value) {
+//     let newElement = document.createElement("ul");
+//     newElement.innerHTML = `${inputs.value} <i class="fa fa-trash-o" style="font-size:24px"></i>`;
+//     text.appendChild(newElement);
+//     inputs.value = "";
+//     newElement.querySelector("i").addEventListener("click", removeElem);
+//     function removeElem() {
+//       newElement.remove();
+//     }
+//   } else {
+//     alert("plz enter  number");
+//   }
+// }
+
+// var userName = "she is my friend manaheel is my first";
+// var texts = userName.indexOf("manaheel");
+// var replace = Math.random()
+// console.log(replace);
+// var text = "my name is manaheel jamil"
+// for (var i = 0; i < text.length; i++) {
+//   if (text.charAt(i) === "!") {
+//     alert("Exclamation point found!");
+//     break;
+//   }else{
+//     alert("not found")
+//     break;
+//   }
+// }
+
+
+// var center = "saylani institute"
+// var newWord = center.slice(0,7)
+// const array = [ "pizza", "pasta", "cold drink", "burger", "zinger"]
+// const newArray = array.slice(4,5)
+// console.log(newArray)
+// const para = "loremmmmmmm";
+// console.log(para.length)
+// var text = "she is my friend and she is also my best friend"
+// var find = text.charAt(text.length-1)
+// console.log(find)
+
+var myInteger = parseFloat("1.9999");
+console.log(typeof myInteger)
+
+var numberAsNumber = 1234;
+// var numberAsString = numberAsNumber.toString();
+console.log(typeof numberAsNumber)
